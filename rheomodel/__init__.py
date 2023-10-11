683051cf21886651cf9695a439b82238c8961c56
marcocaggioni <marco.caggioni@gmail.com>
2023-10-11 02:16:07 -0400

    cleaned readme

--- rheomodel/__init__.py
+++ rheomodel/__init__.py
@@ -4,5 +4,5 @@ rheology model collection
 '''
 __version__ = "0.1"
 
-from . import models
+from .models import HB, TC, carreau, casson, Newtonian, cross, Powerlaw, Bingham
 from .functions import library_to_table, library_to_renderjson, library
